8b535a73d8853f359831c5cd3eb0dd572a581465
50RIDER <99804674+50RIDER@users.noreply.github.com>
2022-09-20 18:52:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MAJORTG2/Eva-marias.git /Eva marias
+  git clone https://github.com/MAJORTG2/Eva-marias.git/Eva marias
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva marias
